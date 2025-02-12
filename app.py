da2e81c8467c959b144087332377bff1d0096b1a
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2025-02-12 08:47:31 +0000

    a

--- app.py
+++ app.py
@@ -1,52 +1,26 @@
-"""
-Streamlit Cheat Sheet
-
-App to summarise streamlit docs v1.25.0
-
-There is also an accompanying png and pdf version
-
-https://github.com/daniellewisDL/streamlit-cheat-sheet
-
-v1.25.0
-20 August 2023
-
-Author:
-    @daniellewisDL : https://github.com/daniellewisDL
-
-Contributors:
-    @arnaudmiribel : https://github.com/arnaudmiribel
-    @akrolsmir : https://github.com/akrolsmir
-    @nathancarter : https://github.com/nathancarter
-
-"""
-
 import streamlit as st
 from pathlib import Path
 import base64
 
 # Initial page config
-
 st.set_page_config(
-     page_title='Streamlit cheat sheet',
-     layout="wide",
-     initial_sidebar_state="expanded",
+     page_title='AI APIs cheat sheet',
+     layout="centered",
+     initial_sidebar_state="collapsed",
 )
 
 def main():
     cs_sidebar()
     cs_body()
-
     return None
 
 # Thanks to streamlitopedia for the following code snippet
-
 def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
     return encoded
 
 # sidebar
-
 def cs_sidebar():
 
     st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
@@ -107,13 +81,12 @@ pip install streamlit-nightly --upgrade
 
 def cs_body():
 
-
-    st.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+    #st.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
     st.header('Streamlit cheat sheet')
 
-    st.markdown('''
-<small>Summary of the [docs](https://docs.streamlit.io/), as of [Streamlit v1.25.0](https://www.streamlit.io/).</small>
-    ''', unsafe_allow_html=True)
+#     st.markdown('''
+# <small>Summary of the [docs](https://docs.streamlit.io/), as of [Streamlit v1.25.0](https://www.streamlit.io/).</small>
+#     ''', unsafe_allow_html=True)
 
     st.markdown('__Mistral AI__')
 
@@ -137,7 +110,6 @@ chat_response = client.chat.complete(
         },
     ]
 )
-
 print(chat_response.choices[0].message.content)
 ''',
     language = "python",
@@ -145,40 +117,33 @@ print(chat_response.choices[0].message.content)
     wrap_lines = True
     )
 
-    st.markdown('__Add widgets to sidebar__')
-    st.code('''
-# Just add it after st.sidebar:
->>> a = st.sidebar.radio(\'Choose:\',[1,2])
-    ''')
 
-    st.markdown('__Magic commands__')
-    st.code('''
-'_This_ is some __Markdown__'
-a=3
-'dataframe:', data
-''')
 
-    st.markdown('__Command line__')
-    st.code('''
-$ streamlit --help
-$ streamlit run your_script.py
-$ streamlit hello
-$ streamlit config show
-$ streamlit cache clear
-$ streamlit docs
-$ streamlit --version
-    ''')
+    st.markdown('__Anthropic__')
 
-    st.markdown('__Pre-release features__')
-    st.code('''
-pip uninstall streamlit
-pip install streamlit-nightly --upgrade
-    ''')
-    st.markdown('<small>Learn more about [experimental features](https://docs.streamlit.io/library/advanced-features/prerelease#beta-and-experimental-features)</small>', unsafe_allow_html=True)
+    st.code('$ pip install anthropic ')
 
-    st.markdown('''<hr>''', unsafe_allow_html=True)
-    st.markdown('''<small>[Cheat sheet v1.25.0](https://github.com/daniellewisDL/streamlit-cheat-sheet)  | Aug 2023 | [Daniel Lewis](https://daniellewisdl.github.io/)</small>''', unsafe_allow_html=True)
+    st.code(body = '''
+import anthropic
 
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key="my_api_key",
+)
+
+message = client.messages.create(
+    model="claude-3-5-sonnet-20240620",
+    max_tokens=8192,
+    temperature=0,
+    system="Your system prompt here",
+    messages=[]
+)
+print(message.content)
+''',
+    language = "python",
+    line_numbers = True,
+    wrap_lines = True
+    )
 
     return None
 
